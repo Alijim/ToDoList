4d56c61ac2cc5a4b53cb9255dca9a34a7c2047e9
hacky76 <hacky76@hotmail.fr>
2020-03-15 20:59:13 +0100

    hvljh

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -3,15 +3,32 @@ package com.example.todolist;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView myRecyclerView;
+    private ArrayList<String> myDataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize the RecyclerView.
+        myRecyclerView = findViewById(R.id.recyclerView);
+        // Set the Layout Manager.
+        myRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // Initialize the ArrayList that will contain the data.
+        myDataList = new ArrayList<>();
+
+        myDataList.add("aaaaaaa");
+        myDataList.add("bbbbbbb");
+        myDataList.add("ccccccc");
 
     }
 
