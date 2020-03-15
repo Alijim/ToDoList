73f26b14b2e39fa65d48fbc0b9a761dbd32cfa4b
hacky76 <hacky76@hotmail.fr>
2020-03-15 17:01:18 +0100

    Commit après 1ère frayeur

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.todolist;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
