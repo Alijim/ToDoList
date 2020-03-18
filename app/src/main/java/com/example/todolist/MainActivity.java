3ed4a23cb924f94c9b6258bc26292a4744a3554b
hacky76 <hacky76@hotmail.fr>
2020-03-18 15:28:34 +0100

    Codelabs: RecyclerView --> Task3 OK

---
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.todolist;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final ArrayList<String> myToDoList = new ArrayList<>();
+    private RecyclerView myRecyclerView;
+    private ToDoListAdapter myAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Toolbar toolbar = this.findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        for (int i=0; i<20; i++) {
+            myToDoList.add("Word " + i);
+        }
+
+        /*myToDoList.add("aaaaaaa");
+        myToDoList.add("bbbbbbb");
+        myToDoList.add("cccccccc");*/
+
+        // Get a handle to the RecyclerView.
+        myRecyclerView = findViewById(R.id.recyclerview);
+        // Create an adapter and supply the data to be displayed.
+        myAdapter = new ToDoListAdapter(this, myToDoList);
+        // Connect the adapter with the RecyclerView.
+        myRecyclerView.setAdapter(myAdapter);
+        // Give the RecyclerView a default layout manager.
+        myRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+}
