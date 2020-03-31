ef593ac258f71c2295d77a8cca320ac757504965
alijim.bouverat <bouveratalij@gmail.com>
2020-03-31 17:52:13 +0200

    Ajout d'un displayTitle

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView displayTitle = findViewById(R.id.titleDisplay);
+        displayTitle.setText("Yeah");
+
+
     }
 }
