cb30f0be137577c24bccd71691af9b95196dc93e
alijim.bouverat <bouveratalij@gmail.com>
2020-04-07 22:05:37 +0200

    Ajout de FeedReaderContract et FeedReaderDbHelper implémentation des méthodes d'insertion permettant l'accès à la base de données

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import database.FeedReaderContract;
+import database.FeedReaderDbHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FeedReaderDbHelper dbHelper = new FeedReaderDbHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(FeedReaderContract.TagsEntry.COLUMN_NAME_WORDING, "coucou");
+        db.insertWithOnConflict(FeedReaderContract.TagsEntry.TABLE_NAME,
+                null,
+                values,
+                SQLiteDatabase.CONFLICT_REPLACE);
+        db.close();
 
         TextView displayTitle = findViewById(R.id.titleDisplay);
         displayTitle.setText("Yeah");
